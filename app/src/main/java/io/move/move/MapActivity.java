7d3d64062950c74f9c589ef18982739a56a6652a
Abilash <abilash432@gmail.com>
2018-06-09 18:33:23 +0530

    9/6/18 6.33 pm
    lelz

--- app/src/main/java/io/move/move/MapActivity.java
+++ app/src/main/java/io/move/move/MapActivity.java
@@ -1,6 +1,10 @@
 package io.move.move;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,42 +12,208 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     GoogleMap mGoogleMap;
+    LocationSettingsRequest.Builder mBuilder;
+    LocationRequest mLocationRequest;
+    SettingsClient mSettingsClient;
+    FusedLocationProviderClient mFusedLocationProviderClient;
+
+    SlidingUpPanelLayout mSlidingUpPanel;
+
+    MarkerOptions markerOptions1;
+    MarkerOptions markerOptions2;
+    MarkerOptions markerOptions3;
+    MarkerOptions markerOptions4;
+    MarkerOptions markerOptions5;
+    MarkerOptions markerOptions6;
+
+    /**
+     *     sample latitudes and longitudes of places as follows:
+            banashankari, whitefield, yeshwantpur, koramangla, electronic city, airport.
+     */
+    double[] latitudes = {12.9255, 12.9698, 13.0280, 12.9279, 12.8407, 13.1986};
+    double[] longitudes = {77.5468, 77.7499, 77.5409, 77.6271, 77.6763, 77.7066};
+
+
+    String TAG = "MapView, MOOVEBABY";
+
+    //last location
+    public double lat;
+    public double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        //mSlidingUpPanel = (SlidingUpPanelLayout) findViewById(R.id.mSlidingUpPanel);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //makes a location request
+        createLocRequest();
+        //makes the LocationSettingsRequest.Builder
+        mBuilder = new LocationSettingsRequest.Builder();
+        mSettingsClient = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = mSettingsClient.checkLocationSettings(mBuilder.build());
+
+
+        //when the location settings are all set and good
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                    //Location settings are active. can get data.
+                    Log.e(TAG, "location settings are OK. MAPVIEW");
+                    pullLastLocationData();
+            }
+        });
+
+        //when the user location settings are not a-OK
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                //location settings not active. reasons be phone's location turned off or no GPS.
+                //TODO respond to user to turn on gps.
+                Log.e(TAG, "location settings are NOT OK. MAPVIEW");
+
+            }
+        });
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private void pullLastLocationData() {
+        //TODO HAVE TO ASK FOR PERMISSION IN THE SPLASH>>>> FOR NOW WE ARE ENABLING IT MANUALLY.
+        Log.e(TAG, "dont forget to ask permission");
+        mFusedLocationProviderClient = new FusedLocationProviderClient(this);
+        mFusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location!= null){
+                    Log.e(TAG, "location INFO are OK. MAPVIEW");
+                    lat =  location.getLatitude();
+                    Log.e(TAG, "Latitude is "+ lat );
+                    longitude =  location.getLongitude();
+                    Log.e(TAG, "Longitude is "+ longitude );
+
+
+
+                    updateMapView();
+                }
+            }
+        });
+
+
+    }
+
+    private void updateMapView() {
+        //gets the last known location, focuses on map.
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        Log.e(TAG, "location settings are OK. NOW MAP IS UPDATING TO THAT LOCATION COORDINATES");
+        MarkerOptions mMarkerOptions = new MarkerOptions().position(new LatLng(lat, longitude));
+        BitmapDescriptor mIcon = BitmapDescriptorFactory.fromResource(R.drawable.user_location);
+        mMarkerOptions.icon(mIcon);
+
+        /**
+         *         TODO BASICALLY THIS IS NOT SUPPOSED TO BE A MARKER>>>
+         *         TODO the actual camera view should be here, continuously updating through a loop
+         *         TODO but im putting that as a marker cuz im an asshole
+         *
+         */
+
+        mGoogleMap.addMarker(mMarkerOptions);
+
+
+
+
+    }
+
+    private void createLocRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setInterval(10000);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Log.e(TAG, "the Map is ready");
         mGoogleMap = googleMap;
-        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);//depends.
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(12.9754, 80.2206)));
+        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);//depends.
+        if(lat != 0 && longitude != 0){
+            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(lat, longitude)));
+            mGoogleMap.setMaxZoomPreference(400);
+            mGoogleMap.setMinZoomPreference(10);
+
+        }else{
+            //mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(12.9754, 80.2206)));
+            //mGoogleMap.setMaxZoomPreference(200);
+            //mGoogleMap.setMinZoomPreference(10);
+
+        }
         /*mGoogleMap.setLatLngBoundsForCameraTarget(new LatLngBounds(new LatLng(13.0012,80.2565),
                 new LatLng(12.9754,80.2206)));//gotta include the city bounds. here it's adyar and vChery*/
 
+        googleMap.setOnMarkerClickListener(this);
+
 
         //make more markeroptions and addMarkers
-        MarkerOptions markerOptions = new MarkerOptions().position(new LatLng(12.9754, 80.2206)).
-                title("firstplace");
-        googleMap.addMarker(markerOptions);
+        markerOptions1 = new MarkerOptions().position(new LatLng(latitudes[0], longitudes[0])).
+                title("banashankari");
+        googleMap.addMarker(markerOptions1);
+
+
+        markerOptions2 = new MarkerOptions().position(new LatLng(latitudes[1], longitudes[1])).
+                title("whitefield");
+        googleMap.addMarker(markerOptions2);
+
+        markerOptions3 = new MarkerOptions().position(new LatLng(latitudes[2], longitudes[2])).
+                title("yeshwantpur");
+        googleMap.addMarker(markerOptions3);
+
+        markerOptions4 = new MarkerOptions().position(new LatLng(latitudes[3], longitudes[3])).
+                title("koramangla");
+        googleMap.addMarker(markerOptions4);
+
+
+
+        markerOptions5 = new MarkerOptions().position(new LatLng(latitudes[4], longitudes[4])).
+                title("Electronic city");
+        googleMap.addMarker(markerOptions5);
+
+        markerOptions6 = new MarkerOptions().position(new LatLng(latitudes[5], longitudes[5])).
+                title("Airport");
+        googleMap.addMarker(markerOptions6);
+
 
     }
 
@@ -97,5 +267,26 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        /**
+         * finish for all markers
+         */
+        if(marker.equals(markerOptions1)){
+            //mSlidingUpPanel.setVisibility(View.VISIBLE);
+        }else if(marker.equals(markerOptions2)){
+            //mSlidingUpPanel.setVisibility(View.VISIBLE);
+
+
+        }else if(marker.equals(markerOptions2)){
+        //mSlidingUpPanel.setVisibility(View.VISIBLE);
+
+
+        }else{
+
+        }
+        return false;
     }
 }
